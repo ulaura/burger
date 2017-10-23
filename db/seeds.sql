d7ada40629ed75fab3f52ac3c46061bd1f26c206
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-23 06:36:33 -0700

    Commiting necessary directories/files. Still need orm.js and MVC setup files.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers_db.burgers(burger_name, devoured) 
+VALUES ("Cheeseburger", false), ("Pizza Burger", false), ("Steakburger", false);
+-- remember that true shows up as 1 and false shows up as 0 in the table.
+
+SELECT * FROM burgers;
---
+++ server.js
@@ -0,0 +1,7 @@
+// Bringing in npm dependencies
+var express = require("express");
+var methodOver = require("method-override");
+var bodyParser = require("body-parser");
+
+
+
