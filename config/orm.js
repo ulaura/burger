ece85196a850b8f407a34093ad13b6584b78e247
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-24 00:34:35 -0700

    Updated orm.js file to include cb().

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,37 @@
 var connection = require("./connection.js");
 
 var orm = {
-	selectAll: function(tableInput) {
+	// "cb" helps with asynchronous issues. It forces JS to wait for
+	// the data to return from the database before moving on to the 
+	// next part of the method
+	selectAll: function(tableInput, cb) {
 		var queryString = "SELECT * FROM ??";
 		connection.query(queryString, [tableInput], function(err, result){
 			if (err) {
 				throw err;
 			}
 			console.log(result);
+			cb(result);
 		});
 	},
-	insertOne: function(tableInput, valOfCol) {
+	insertOne: function(tableInput, valOfCol, cb) {
 		var queryString = "INSERT INTO ?? VALUES(?)";
 		connection.query(queryString, [tableInput, valOfCol], function(err, result) {
+			if (err) {
+				throw err;
+			}
 			console.log(result);
+			cb(result);
 		});
 	},
-	updateOne: function(tableInput, columnInput, valOfCol, condition, valOfCondition) {
+	updateOne: function(tableInput, columnInput, valOfCol, condition, valOfCondition, cb) {
 		var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
 		connection.query(queryString, [tableInput, columnInput, valOfCol, condition, valOfCondition], function(err, result) {
+			if (err) {
+				throw err;
+			}
 			console.log(result);
+			cb(result);
 		});
 	}
 };
