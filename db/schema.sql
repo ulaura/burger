eecf278e0186b2704d4fc7477e0272d4f7aeefad
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-24 06:34:24 -0700

    Set default of devoured BOOLEAN to false.

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,8 @@ USE burgers_db;
 CREATE TABLE burgers (
 id INT NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(250) NOT NULL,
-devoured BOOLEAN NOT NULL,
+devoured BOOLEAN DEFAULT false,
 date TIMESTAMP,
 PRIMARY KEY (id)
 );
+
