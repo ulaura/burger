c17394b2cda773dc225a6fdf3e35ca06ce7a2d58
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-24 18:37:00 -0700

    Added a PUT request on burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -42,4 +42,23 @@ router.post("/api/burgers", function(req, res) {
 
 // PUT request updates data.
 // This is where the burger goes from "not devoured" to "devoured"
+router.put("/api/burgers/:id", function(req, res) {
+	var burgerId = "id = " + req.params.id;
 
+	console.log("Burger status changed for ", devoured);
+
+	burger.updateOne({
+		devoured: req.body.devoured
+	}, burgerId, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+// Export routes for use on server.js
+module.exports = router;
