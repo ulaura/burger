9ea18cc15733c2ab15fe58ffa9e0f2edfe575022
Laura Unaeze <l.unaeze5@gmail.com>
2017-12-28 16:48:48 -0700

    Modified connection.js for use on Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 // Connecting Node to burgers_db in MySQL 
 // and then exporting the connection
-var mysql      = require('mysql');
+var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Database!DreamGirl28',
-  database : 'burgers_db'
-});
+// Setting up connection for use on Heroku
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+// Localhost connection
+else {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Database!DreamGirl28',
+    database : 'burgers_db'
+  });
+}
 
+connection.connect();
 module.exports = connection; 
